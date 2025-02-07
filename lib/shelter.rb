af4307a2db8190027810c3cb2dc1d08a799e9e82
Katya Weicht <katya.weicht@gmail.com>
2025-02-07 15:07:01 -0700

    Finish independent challenge, iterations 1-4

--- lib/shelter.rb
+++ lib/shelter.rb
@@ -1,2 +1,33 @@
 class Shelter
+    attr_reader :name, :capacity, :pets
+    def initialize(name,capacity)
+        @name = name
+        @capacity = capacity
+        @pets = []
+
+    end
+
+    def add_pet(pet)
+        @pets << pet
+    end
+
+    def call_pets
+        @pets.map {|pet| "#{pet}!"}
+    end
+
+    def over_capacity?
+        if @pets.length <= @capacity
+            false
+        else
+            true
+        end
+    end
+
+    def adopt
+        n = @pets.length - @capacity
+        n.times do 
+            @pets.shift
+        end  
+    end
+
 end
