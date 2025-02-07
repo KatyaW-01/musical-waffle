4555aea3f98338f9f554e1f4022b9395ac8c90da
Katya Weicht <katya.weicht@gmail.com>
2025-02-07 15:08:06 -0700

    Finish all tests for independent challenge

--- spec/shelter_spec.rb
+++ spec/shelter_spec.rb
@@ -5,22 +5,22 @@ RSpec.describe Shelter do
 
   # Iteration 1
   describe '#initialize' do
-    xit 'is a Shelter' do
+    it 'is a Shelter' do
       shelter = Shelter.new('Denver Animal Shelter', 5)
       expect(shelter).to be_a(Shelter)
     end
 
-    xit 'can read the name' do
+    it 'can read the name' do
       shelter = Shelter.new('Denver Animal Shelter', 5)
       expect(shelter.name).to eq('Denver Animal Shelter')
     end
 
-    xit 'can read the capacity' do
+    it 'can read the capacity' do
       shelter = Shelter.new('Denver Animal Shelter', 5)
       expect(shelter.capacity).to eq(5)
     end
 
-    xit 'has no pets by default' do
+    it 'has no pets by default' do
       shelter = Shelter.new('Denver Animal Shelter', 5)
       expect(shelter.pets).to eq []
     end
@@ -28,7 +28,7 @@ RSpec.describe Shelter do
 
   # Iteration 2
   describe '#add_pet' do
-    xit 'returns a list of pets' do
+    it 'returns a list of pets' do
       shelter = Shelter.new('Denver Animal Shelter', 5)
       shelter.add_pet('Salem')
       shelter.add_pet('Beethoven')
@@ -40,7 +40,7 @@ RSpec.describe Shelter do
   end 
 
   describe '#call_pets' do
-      xit 'returns a list of names with exclamation points appended' do
+      it 'returns a list of names with exclamation points appended' do
         shelter = Shelter.new('Denver Animal Shelter', 5)
         shelter.add_pet('Salem')
         shelter.add_pet('Beethoven')
@@ -51,3 +51,46 @@ RSpec.describe Shelter do
       end
     end
 end
+
+  #Iteration 3
+  describe '#over_capacity?' do
+    it 'returns false if within capacity' do
+      shelter = Shelter.new('Denver Animal Shelter', 3)
+
+      shelter.add_pet('Salem')
+      shelter.add_pet('Beethoven')
+
+      expect(shelter.over_capacity?).to eq(false)
+
+    end
+
+    it 'returns true if at exceeds capacity' do
+      shelter = Shelter.new('Denver Animal Shelter', 3)
+
+      shelter.add_pet('Salem')
+      shelter.add_pet('Beethoven')
+      shelter.add_pet('Spot')
+      shelter.add_pet('Jonesy')
+
+      expect(shelter.over_capacity?).to eq(true)
+    end
+
+  end
+
+  #Iteration 4
+
+  describe '#adopt' do
+    it 'removes animals until no longer over capacity' do
+      shelter =Shelter.new("Denver Animal Shelter", 3)
+      shelter.add_pet('Salem')
+      shelter.add_pet('Beethoven')
+      shelter.add_pet('Spot')
+      shelter.add_pet('Jonesy')
+
+      shelter.adopt
+
+      expect(shelter.over_capacity?).to eq(false)
+      expect(shelter.pets).to eq(['Beethoven', 'Spot', 'Jonesy'])
+      
+    end
+  end
